1128fdaee02d9024481564bbeeab0c867ce64549
Paul Arce <pauarce@publicisgroupe.net>
2019-07-07 19:57:32 -0400

    Replacing placeholder pages

--- src/styleguide/containers/FootersContainer.jsx
+++ src/styleguide/containers/FootersContainer.jsx
@@ -15,55 +15,11 @@ export default class FootersContainer extends React.Component {
    */
   render() {
     return (
-      <DocsPage title="Footer">
-        
-        {/* If multiple component or component groups on the page, wrap each in a DocsComponent */}
-        <DocsComponent title="Component name" id="XXXX_THE_ID_XXXX">
-        
-          {/* Dependencies */}
-          <DocsSection title="Dependencies">
-            <h5>Explain what the 3rd-party dependency is and what it does</h5>
-            <Highlight className="bash">
-              {`npm install --save react-thirdpartything`}
-            </Highlight>
-          </DocsSection>
-          
-          {/* Importing */}
-          <DocsSection title="Importing">
-            <Highlight className="javascript">
-              {`import XXXX from '.%path%/uswds/components/XXXX';`}
-            </Highlight>
-          </DocsSection>
-          
-          {/* Props */}
-          <DocsSection title="Props">
-            <Table columns={['Prop', 'Type', 'Default', 'Description']}>
-              <TableRow>
-                <TableHeaderCell>propName<br /><Tag>Required</Tag></TableHeaderCell>
-                <TableCell>bool</TableCell>
-                <TableCell>false</TableCell>
-                <TableCell>What_it_does</TableCell>
-              </TableRow>
-            </Table>
-          </DocsSection>
-          
-          {/* Accessibility */}
-          <DocsSection title="Accessibility"></DocsSection>
-          
-          {/* Usage */}
-          <DocsSection title="Usage">
-            <DocsExample>
-              <DocsPreview>
-                Rendered_example_here
-              </DocsPreview>
-              <Highlight className="html">
-                {`<YourCode>Goes here</YourCode`}
-              </Highlight>
-            </DocsExample>
-          </DocsSection>
-          
-        </DocsComponent>
-      </DocsPage>
+      <React.Fragment>
+        <h1>Footers</h1>
+    
+        <p> For the Footer code and its variants, visit <a href="https://components.designsystem.digital.gov/" target="_blank" rel="noopener noreferrer">components.designsystem.digital.gov <span className="usa-sr-only">opens in new window</span></a>.</p>
+      </React.Fragment>
     );
   }
 }
